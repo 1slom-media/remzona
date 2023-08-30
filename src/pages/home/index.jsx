d1eef81e5ceb2bd54eaef9488baa3a7a9bbffe0b
1slom_media <islombektagayev1@gmail.com>
2023-08-30 23:41:02 +0500

    add seo sitemap

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,29 @@
-import React, {useEffect} from 'react'
-import HomeComponent from '../../components/home'
+import React, { useEffect } from "react";
+import HomeComponent from "../../components/home";
+import { useDispatch, useSelector } from "react-redux";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const Home = () => {
+  const dispatch = useDispatch();
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
+
+  const Data = useSelector((state) => state.seo.SeoGet.data);
   return (
     <>
-      <HomeComponent/>
+      <HelmetProvider>
+        {Data.map((elem) => (
+          <Helmet>
+            <title>{elem.meta_title}</title>
+            <meta name={elem.meta_key} content={elem.meta_description} />
+            <link rel="cononical" href="/" />
+          </Helmet>
+        ))}
+        <HomeComponent />
+      </HelmetProvider>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import ContactSlice from './contact/index';
 import PortfolioSlice from './portfolio/index'
 import BlogSlice from './blog/index'
+import SeoSlice from "./seo/index";
 export const store = configureStore({
   reducer: {
     contact: ContactSlice,
     portfolio: PortfolioSlice,
-    blog: BlogSlice
+    blog: BlogSlice,
+    seo:SeoSlice,
   },
 });
