9ff6047151f10c24295c7e7ef13c01507a67b98c
1slom_media <islombektagayev1@gmail.com>
2023-09-05 13:27:57 +0500

    add yandex metrika

--- src/router/index-data.js
+++ src/router/index-data.js
@@ -1,11 +1,17 @@
 import { lazy } from "react";
 
-const Home = lazy(() => import("../pages/home/index"))
+const Home = lazy(() => import("../pages/home/index"));
+const ErrorPage = lazy(() => import("../pages/Error/index"));
 
 export const RouterData = [
-    {
-        id : 1,
-        path : "/",
-        component : <Home/>
-    }
-]
+  {
+    id: 1,
+    path: "/",
+    component: <Home />,
+  },
+  {
+    id: 2,
+    path: "/*",
+    component: <ErrorPage />,
+  },
+];
